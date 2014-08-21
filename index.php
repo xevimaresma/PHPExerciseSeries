7b1b82e462a9e449ac3300f2b6c33dc909f1c174
Xevi Maresma Ridorsa <xevi@cdmon.com>
2014-08-21 12:52:52 +0200

    Initial Commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<html>
+<head>
+  <title>Example Old Paradigm Programming</title>
+</head>
+<body>
+<style type="text/css">
+  p.error {
+    border-width: 1px;
+    border-color: red;
+    color: red;
+  }
+  p.reverse {
+    border-width: 1px;
+    border-color: green;
+    color: green;
+  }
+</style>
+<?php
+  if($_SERVER['REQUEST_METHOD']=='POST'){
+    $name = $_POST['name'];
+    //Error handling
+    if ($name == "") {
+      ?>
+       <p class="error">There was an error in your input</p>
+      <?php
+    }
+
+    //Reverse name
+    $result = strrev($name);
+  }
+?>
+
+  <form name="form1" action="index.php" method="post">
+    <label for="name">Name: </label><input type="text" name="name" value="<?=$name?>"/>
+    <input type="submit" value="Send" />
+  </form>
+
+<?php
+  if($result){
+    echo "<p class=\"reverse\">Your reversed string is: $result</p>";
+  }
+?>
+</body>
+</html>
